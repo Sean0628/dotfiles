2bc17d94280286d0d288cb05167b020851249c7c
Sho Ito <i.sho.0628@gmail.com>
2018-06-27 00:13:00 +0900

    add sentence about rbenv

--- config.fish
+++ config.fish
@@ -1,13 +1,16 @@
 # Set vim as a bundler editor
 set -x BUNDLER_EDITOR vim
 
+# plugins
 set fish_plugins theme peco
-ulimit -n 9480
 
 # Bind for peco history to Ctrl+r
 function fish_user_key_bindings
   bind \cr peco_select_history
 end
+
+# rbenv setup
+status --is-interactive; and . (rbenv init -|psub)
 
 # Balias
 balias rr 'rake routes'
@@ -15,9 +18,14 @@ balias rs 'rails s'
 balias rt 'rails t'
 balias rc 'rails c'
 balias cd... 'cd ../..'
+balias g 'git'
 balias gb 'git branch'
 balias ga 'git add'
 balias gc 'git commit'
 balias gco 'git checkout'
 balias gd 'git diff'
 balias gs 'git status'
+
+# other
+ulimit -n 9480
+
