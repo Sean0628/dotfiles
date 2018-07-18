90a29cc636abbb4ce31ac6319dd0eeb8cdbbe5e3
Sho Ito <i.sho.0628@gmail.com>
2018-07-19 00:02:06 +0900

    add bash config files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
