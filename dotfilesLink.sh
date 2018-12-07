a5eeb7c46e37324ef1b311731b9ad917f2771ca7
sean0628 <i.sho.0628@gmail.com>
2018-12-07 11:09:12 +0900

    mod indentation

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.vimrc            ~/.vimrc
+ln -sf ~/dotfiles/config.fish       ~/.config/fish/config.fish
+ln -sf ~/dotfiles/.tmux.conf        ~/.tmux.conf
+ln -sf ~/dotfiles/.bash_profile     ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc           ~/.bashrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.ctags ~/.ctags
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/.ctags            ~/.ctags
+ln -sf ~/dotfiles/.ideavimrc        ~/.ideavimrc
