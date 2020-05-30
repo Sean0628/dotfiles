460be5593ca1e2cc00a3ce6f2b943d4515880ea7
Sho ITO <i.sho.0628@gmail.com>
2020-05-30 16:32:19 +0900

    Mod install script

--- brew_install.sh
+++ brew_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 function command_exists {
   command -v "$1" > /dev/null;
 }
@@ -7,38 +7,15 @@ function gem_exists {
   gem list -i "$1" > /dev/null;
 }
 
-if ! command_exists nvim ; then
-  brew install neovim
-fi
+declare -A commandList=( \
+  [nvim]=neovim [ag]=the_silver_searcher \
+  [ctags]=ctags [CMake]=CMake [tmux]=tmux \
+  [rg]=ripgrep [autojump]=autojump [ghq]=ghq \
+  [jq]=jq
+  )
 
-if ! command_exists ag ; then
-  brew install the_silver_searcher
-fi
-
-if ! command_exists ctags ; then
-  brew install ctags
-fi
-
-if ! command_exists zsh ; then
-  brew install zsh
-fi
-
-if ! command_exists CMake ; then
-  brew install CMake
-fi
-
-if ! command_exists tmux ; then
-  brew install tmux
-fi
-
-if ! command_exists rg ; then
-  brew install ripgrep
-fi
-
-if ! command_exists autojump ; then
-  brew install autojump
-fi
-
-if ! command_exists ghq ; then
-  brew install ghq
-fi
+for key val in ${(kv)commandList}; do
+	if ! command_exists $key ; then
+    brew install $val
+  fi
+done
